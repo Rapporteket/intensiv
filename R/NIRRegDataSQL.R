e9d511c57a31f5fdcfa888a68ec178f3545e6b12
Olsen Lena Ringstad <lro2402unn@hn.helsenord.no>
2016-06-16 16:22:43 +0200

    Med PasientGUID

--- R/NIRRegDataSQL.R
+++ R/NIRRegDataSQL.R
@@ -40,6 +40,7 @@ NIRRegDataSQL <- function(datoFra = '2011-01-01', datoTil = '2099-01-01') {
 	OrganDonationCompletedStatus,
 	PatientAge,
 	PatientGender,
+      PasientGUID,
 	PatientTransferredFromHospital,
 	PatientTransferredToHospital,
 	Potassium,
