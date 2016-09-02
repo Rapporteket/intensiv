a7ecec24a50a0bef80f5ebed56932ce21fc95275
Are Edvardsen <are.edvardsen@helse-nord.no>
2016-09-02 08:33:09 +0200

    first test of a static hc

---
+++ R/NIRHighcharts.R
@@ -0,0 +1,18 @@
+#' Test of Highchart for intensiv
+#' 
+#' asæglksa
+#' 
+#' @param outfile String providing fully qualified path and name of html widget
+#' @export
+
+AndelerHighchart <- function(outfile) {
+  
+  # just for testing
+  h1 <- highcharter::highchart() %>% 
+    hc_chart(type = "column") %>% 
+    hc_title(text = "A highcharter chart") %>% 
+    hc_xAxis(categories = 2012:2016) %>% 
+    hc_add_series(data = c(3900,  4200,  5700,  8500, 11900),
+                  name = "Downloads")
+  htmlwidgets::saveWidget(h1, outfile, selfcontained = TRUE)
+}
