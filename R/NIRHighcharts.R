47a08f25b121f0bb32f8ab310a3165acd90f38c6
Are Edvardsen <are.edvardsen@helse-nord.no>
2016-09-02 11:56:45 +0200

    expose all functions of highcharter

--- R/NIRHighcharts.R
+++ R/NIRHighcharts.R
@@ -4,12 +4,13 @@
 #' 
 #' @param outfile String providing fully qualified path and name of html widget
 #' @importFrom magrittr "%>%"
+#' @import highcharter
 #' @export
 
 AndelerHighchart <- function(outfile) {
   
   # just for testing
-  h1 <- highcharter::highchart() %>% 
+  h1 <- highchart() %>% 
     hc_chart(type = "column") %>% 
     hc_title(text = "A highcharter chart") %>% 
     hc_xAxis(categories = 2012:2016) %>% 
