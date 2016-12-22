f65e1f06cc1ccc5f447fc9718604dc717413b993
Are Edvardsen <are.edvardsen@helse-nord.no>
2016-12-22 13:02:38 +0100

    first nonworking ui

---
+++ inst/shinyApps/report/ui.R
@@ -0,0 +1,35 @@
+# This is the user-interface definition for the 'report' Shiny web application
+# for 'Nakke'
+
+require(highcharter)
+data("RegData")
+
+# not implemented just yet, wait until sample data without preprocessing
+if (1==0) {
+  RegData <- NakkePreprosess(RegData=RegData)
+}
+
+shinyUI(
+  navbarPage(title = "INTENSIVREGISTERET", theme = "bootstrap.css",
+             tabPanel("Andeler",
+                      sidebarLayout(
+                        sidebarPanel(
+                          selectInput("andelerValgtVar", "Variabel:",
+                                      valgtVarFigAndeler(),
+                                      selected = "Alder"),
+                          nakkeStandardInput("figAndeler"),
+                          downloadButton("downloadData",
+                                         label = "Last ned data")
+                        ),
+                        mainPanel(tabsetPanel(
+                          tabPanel("Figur",
+                                   highchartOutput("andelerPlot")
+                          ),
+                          tabPanel("Data",
+                                   DT::dataTableOutput("andelerTable")
+                          )
+                        ))
+                      )
+             )
+  )
+)
