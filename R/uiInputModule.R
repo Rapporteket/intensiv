b95761a3bf6df7850d951d24ed0b1dbbb5e64042
Are Edvardsen <are.edvardsen@helse-nord.no>
2016-12-22 13:24:50 +0100

    some content...

---
+++ R/uiInputModule.R
@@ -0,0 +1,31 @@
+#' Modulised UI for shiny
+#'
+#' uiInputModule provides common ui uc for shiny intensiv
+#'
+#'  @param id namespace for this module
+#'  @param label Label for this module
+#'  @export
+
+uiInputModule <- function(id, label = "Brukervalg") {
+  
+  # create namespace
+  ns <- NS(id)
+  
+  # make values and lables for reshID
+  reshList <-
+    setNames(as.list(unique(RegData$AVD_RESH)), unique(RegData$SykehusNavn))
+  
+  tagList(
+    selectInput(ns("erMann"),
+                label="Kjønn:",
+                c("Begge"=2, "Menn"=1, "Kvinner"=0)
+    ),
+    sliderInput(ns("alder"), label = "Alder", min = 0,
+                max = 130, value = c(0, 130)
+    ),
+    dateRangeInput(ns("periode"), start = "2012-01-01", end = Sys.Date(),
+                   label = "Periode", separator="til", language="nb"),
+    selectInput(ns("avdeling"), label = "Avdeling", reshList)
+    #downloadButton(ns("downloadData"), label = "Last ned data")
+  )
+}
