e4d09b1cba928030885c4dfd82eeb14f718b89f5
Are Edvardsen <are.edvardsen@helse-nord.no>
2016-12-29 13:09:29 +0100

    start experimenting on static data and report objects...

--- R/reportObjects.R
+++ R/reportObjects.R
@@ -26,3 +26,58 @@ emptyReport <- function(Tittel, utvalg = "", infoText = "Tomt...") {
   
   list(plotObj=h1, tableObj=t1)
 }
+
+
+#' Provider of report objects
+#' 
+#' Provide report object fractions of readmittance within 72 hours
+#' 
+#' @param Tittel String Title to show in empty chart
+#' @param infoText String Text to shown in empty chart
+#' @return plotObj Highchart object
+#' @return tableObj Table object
+#' @export
+
+readmission72hours <-  function() {
+  
+  # get (static) data
+  data("AndelerGrVarData")
+  
+  # to use extra data in tooltips, make a data series from data frame
+  df <- data.frame(y = as.vector(AndelerGrVarData$AggVerdier$Hoved),
+                   N = as.vector(AndelerGrVarData$Ngr$Hoved),
+                   stringsAsFactors = FALSE)
+  ds <- rlist::list.parse(df)
+  names(ds) <- NULL
+  
+  h1 <- highcharter::highchart() %>%
+    hc_title(text = AndelerGrVarData$tittel) %>%
+    hc_subtitle(text = AndelerGrVarData$utvalgTxt) %>%
+    hc_xAxis(categories=AndelerGrVarData$grtxt,
+             reversed = FALSE) %>%
+    hc_yAxis(title = list(text=AndelerGrVarData$xAkseTxt),
+             min = -0.01,
+             startOnTick = FALSE) %>%
+    hc_add_series(name = "Andeler",
+                  data = ds,
+                  type = "bar",
+                  color = farger[3],
+                  tooltip = list(pointFormat='<b>Andel:</b>
+                                 {point.y:.1f}<br><b>N:</b>
+                                 {point.N}<br/>')) %>%
+    hc_exporting(enabled = TRUE)
+  
+  # add global ratio, later then...
+#   h1 <- hc_add_series(h1, name = paste0("Hele landet (", sprintf('%.1f',
+#                                                                  AndelHele),
+#                                         " %), N=", N),
+#                       data = rep(AndelHele, length(GrNavnSort)),
+#                       type = "line",
+#                       color = farger[2],
+#                       marker = list(enabled=FALSE),
+#                       enableMouseTracking = FALSE
+#   )
+  t1 <- list()
+  
+  list(plotObj=h1, tableObj=t1)
+}
