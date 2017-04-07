3147866a0fe8ce4da168aa729fb7dd9bdcdfb81a
Are Edvardsen <are.edvardsen@helse-nord.no>
2017-04-07 13:04:23 +0200

    move depending ucs into server module

--- R/serverModule.R
+++ R/serverModule.R
@@ -14,11 +14,32 @@ serverModule <- function(input, output, session) {
   # namespace id comes with an extra '-name'. Remove it
   sessionName <- gsub("-name", "", sessionName)
   
+  # observe to catch changes in depending uc at client
+  hospitalType <- reactive({
+    if (is.null(input$hospitalType)) {
+      return(NULL)
+    } else {
+      return(input$hospitalType)
+    }
+  })
+  
+  observe({
+    h_type <- hospitalType()
+    if (!is.null(h_type)) {
+      c_subset <- dplyr::filter(reinnData$RegData, ShType == h_type) %>% 
+        dplyr::distinct(ShNavn)
+      
+      updateSelectInput(session, "hospital",
+                        choices = c_subset$ShNavn,
+                        selected = c_subset$ShNavn)
+    }
+    
+  })
   
   if (sessionName == "readmission72hours") {
     reportObj <- reactive({
       readmission72hours(selectErMann = as.numeric(input$erMann),
-                         selectHospitalType = as.numeric(input$hospitalType))
+                         selectHospital = as.character(input$hospital))
     })
   }
   
