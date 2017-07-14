bc4dba6990b2c8f0a02fd6f69cb31ad53cfff239
Are Edvardsen <are.edvardsen@helse-nord.no>
2017-07-14 11:02:15 +0200

    filtering moved

--- R/serverModule.R
+++ R/serverModule.R
@@ -41,18 +41,71 @@ serverModule <- function(input, output, session) {
   
   if (sessionName == "readmission72hours") {
     reportObj <- reactive({
-      readmission72hours(selectYear = as.numeric(input$year),
-                         selectQuarter = as.numeric(input$quarter),
-                         selectHospital = as.character(input$hospital),
-                         selectHospitalType = as.numeric(input$hospitalType),
-                         selectErMann = as.numeric(input$erMann),
-                         selectAgeGroup = as.character(input$ageGroup))
+      
+      ## do data filtering here and replace by call to generic report function
+      fRegData <- NIRdata01reinn$NIRRegData01Off
+      
+      # the inputs
+      selectYear <- as.numeric(input$year)
+      selectQuarter <- as.numeric(input$quarter)
+      selectHospital <- as.character(input$hospital)
+      selectHospitalType <- as.numeric(input$hospitalType)
+      selectErMann <- as.numeric(input$erMann)
+      selectAgeGroup <- as.character(input$ageGroup)
+      
+      # apply all filters for RegData and make
+      fRegData <- dplyr::filter(fRegData, ShNavn %in% selectHospital &
+                                  Aar %in% selectYear & Kvartal %in% selectQuarter &
+                                  AldersGr %in% selectAgeGroup)
+      
+      # Here: replace NIRRegData01Off with fRegData, all to be sent into NirAnderleGrVar
+      RegData <- NIRdata01reinn
+      RegData$NIRRegData01Off <- fRegData
+      
+      # in case filtering makes empty data
+      if (is.data.frame(fRegData) && nrow(fRegData) == 0) {
+        emptyReport(Tittel = fRegData$tittel, infoText = "Ingen data")
+      } else {
+        shinyReport(RegData, valgtVar = "reinn", selectErMann = selectErMann)
+      }
+      
+      # readmission72hours(selectYear = as.numeric(input$year),
+      #                    selectQuarter = as.numeric(input$quarter),
+      #                    selectHospital = as.character(input$hospital),
+      #                    selectHospitalType = as.numeric(input$hospitalType),
+      #                    selectErMann = as.numeric(input$erMann),
+      #                    selectAgeGroup = as.character(input$ageGroup))
     })
   }
   
   if (sessionName == "gjsnGrVar") {
     reportObj <- reactive({
-      gjsnGrVar()
+      ## do data filtering here and replace by call to generic report function
+      fRegData <- NIRdata01respiratortid$NIRRegData01Off
+      
+      # the inputs
+      selectYear <- as.numeric(input$year)
+      selectQuarter <- as.numeric(input$quarter)
+      selectHospital <- as.character(input$hospital)
+      selectHospitalType <- as.numeric(input$hospitalType)
+      selectErMann <- as.numeric(input$erMann)
+      selectAgeGroup <- as.character(input$ageGroup)
+      
+      # apply all filters for RegData and make
+      fRegData <- dplyr::filter(fRegData, ShNavn %in% selectHospital &
+                                  Aar %in% selectYear & Kvartal %in% selectQuarter &
+                                  AldersGr %in% selectAgeGroup)
+      
+      # Here: replace NIRRegData01Off with fRegData, all to be sent into NirAnderleGrVar
+      RegData <- NIRdata01respiratortid
+      RegData$NIRRegData01Off <- fRegData
+      
+      # in case filtering makes empty data
+      if (is.data.frame(fRegData) && nrow(fRegData) == 0) {
+        emptyReport(Tittel = fRegData$tittel, infoText = "Ingen data")
+      } else {
+        shinyReport(RegData, valgtVar = "respStotte", selectErMann = selectErMann)
+      }
     })
   }
   
