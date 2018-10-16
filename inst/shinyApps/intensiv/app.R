f9966bf7dc768ac6445c482b26e4baf693b19ee9
Olsen Lena Ringstad <lro2402unn@hn.helsenord.no>
2018-10-16 13:22:51 +0200

    Ny struktur i app

--- inst/shinyApps/intensiv/app.R
+++ inst/shinyApps/intensiv/app.R
@@ -34,24 +34,24 @@ library(knitr)
 # Define UI for application that draws figures
 ui <- navbarPage( #fluidPage( #"Hoved"Layout for alt som vises på skjermen
       title = 'NORSK INTENSIVREGISTER',
-      tabPanel("Viktigste resultater",
+      tabPanel("Viktigste resultater/Oversiktsside",
                #fluidRow(
                #column(width=5,
                h2("Månedsrapport"), #),
-               #column(width=2,
-               #downloadButton(outputId = 'mndRapp.pdf', label='FUNKER pt IKKE', class = "butt"),
-               #tags$head(tags$style(".butt{background-color:#6baed6;} .butt{color: white;}")), # background color and font color
-               #)),
+               downloadButton(outputId = 'mndRapp.pdf', label='Månedsrapport', class = "butt"),
+               tags$head(tags$style(".butt{background-color:#6baed6;} .butt{color: white;}")), # background color and font color
                br(),
                br(),
                br(),
                br(),
                br(),
-               h2("Her kan det komme ei startside med de variable/resultater som er viktigst å overvåke", align='center' ),
-               h2("Gi tilbakemelding på hva man ønsker her.", align='center' ),
+               h2("Her kan man evt. vise de variable/resultater som er viktigst å overvåke", align='center' ),
+               h2("Gi tilbakemelding på hva som skal være på sida", align='center' ),
                br()),
       
       tabPanel("Registreringsoversikter",
+               h3('Her kan vi evt. ha "undersider" for å velge hvilken tabell man ønsker'),
+               br(),
                #h3("1. Antall opphold + pasienter per sykehus og år"),
                #h3("2. Antall opphold + pasienter per sykehus og mnd, siste 12 mnd. ")
                h2("Belegg"),
@@ -59,10 +59,12 @@ ui <- navbarPage( #fluidPage( #"Hoved"Layout for alt som vises på skjermen
                
                h2("Antall registreringer per måned og avdeling"),
                p(em("Velg tidsperiode ved å velge sluttdato i menyen til venstre")),
-               tableOutput("tabAvdMnd12")
+               tableOutput("tabAvdMnd12"),
                
-               #          h2("Antall registreringer per år og avdeling, siste 5 år"),
+               h2("Antall registreringer per år og avdeling, siste 5 år"),
                #          #tableOutput("tabAvdNAar5")
+               h2("Moglege dobbeltregistreringar")
+               
       ),
       
       
@@ -248,9 +250,8 @@ ui <- navbarPage( #fluidPage( #"Hoved"Layout for alt som vises på skjermen
       
       tabPanel("Under utvikling",
                br(),
-               h3("HN-IKT-rapporter:"),
-               h3("3. Dobbeltregistreringar"),
                h3("Resultater fra pårørendeskjema"),
+               h3("HN-IKT-rapporter:"),
                h3("4. \"Månedlig oversikt:\" Div. nøkkeltall for egen avd/hele landet, siste 12 måneder"),
                h3("5. Pasienter utskrevet mellom 17:00 og 08:00, helligdager beh. som vanlige dager"),
                h3('6. Potensielle donorer'),
@@ -265,7 +266,7 @@ ui <- navbarPage( #fluidPage( #"Hoved"Layout for alt som vises på skjermen
 
 
 #----------------- Define server logic ----------
-server <- function(input, output, session) {
+server <- function(input, output) { #, session
       
       library(intensiv)
       library(lubridate)
@@ -279,11 +280,11 @@ server <- function(input, output, session) {
       
       if (!exists('RegData')){
             #system.file('inst/IntensivMndRapp.Rnw', package='intensiv')
-      load('A:/Intensiv/NIRdata10000.Rdata')
-      #RegData <- read.table(fil, sep=';', header=T, encoding = 'UTF-8')
-      #Funker:
-      #  data('NIRRegDataSyn', package = 'intensiv')
-      #try(data(package = "intensiv"))
+            load('A:/Intensiv/NIRdata10000.Rdata')
+            #RegData <- read.table(fil, sep=';', header=T, encoding = 'UTF-8')
+            #Funker:
+            #  data('NIRRegDataSyn', package = 'intensiv')
+            #try(data(package = "intensiv"))
       }
       datoTil <- as.POSIXlt(Sys.Date())
       AarNaa <- as.numeric(format(Sys.Date(), "%Y"))
@@ -313,8 +314,8 @@ server <- function(input, output, session) {
             tabAntOpphSh12mnd(RegData = RegData, datoTil=input$datovalgTab[2])  
             #sprintf('%1.3f'
             #xtable::xtable(tabAvd12MndNskjema,  align = c('l', rep('r', ncol(tabAvd12MndNskjema))),
-             #              caption= paste0('Tidsperiode: ', as.POSIXlt(datoFra12), 'til', as.POSIXlt(input$datoTil)))
-      #},
+            #              caption= paste0('Tidsperiode: ', as.POSIXlt(datoFra12), 'til', as.POSIXlt(input$datoTil)))
+            #},
       }, rownames = T, align= 'r' #
       ) 
       
@@ -336,18 +337,13 @@ server <- function(input, output, session) {
             #content = function(file) file.copy(system.file('NIRMndRapp.pdf', package = 'Nakke'), file, overwrite = TRUE),
             content = function(file) {
                   # permission to the current working directory
-                  src <- normalizePath(system.file('NakkeMndRapp.Rnw', package='Nakke'))
+                  src <- normalizePath(system.file('NIRmndRapp.Rnw', package='intensiv'))
                   owd <- setwd(tempdir())
                   on.exit(setwd(owd))
-                  file.copy(src, 'NakkeMndRapp.Rnw', overwrite = TRUE)
+                  file.copy(src, 'NIRmndRapp.Rnw', overwrite = TRUE)
                   
-                  texfil <- knitr::knit(system.file('IntensivMndRapp.Rnw', package='intensiv'), encoding = 'UTF-8')
-                  texi2pdf(system.file(texfil, package='Nakke'),clean = TRUE) #"NakkeMndRapp.tex"
-                  # #help(render_latex)
-                  #       out = system.file('NakkeMndRapp.pdf', package = 'Nakke')
-                  #knit2pdf(system.file('NakkeMndRapp.Rnw', package='Nakke'), clean = TRUE, encoding = 'UTF-8')
-                  #      file.rename(out, file) # move pdf to file for downloading
-                  #file.copy(system.file('NakkeMndRapp.pdf', package='Nakke'), file)
+                  texfil <- knitr::knit(system.file('NIRmndRapp.Rnw', package='intensiv'), encoding = 'UTF-8')
+                  texi2pdf(system.file(texfil, package='intensiv'),clean = TRUE) #"NakkeMndRapp.tex"
                   file.copy('NakkeMndRapp.pdf', file)
                   
             },
@@ -366,7 +362,7 @@ server <- function(input, output, session) {
                           datoFra=input$datovalg[1], datoTil=input$datovalg[2],
                           minald=as.numeric(input$alder[1]), maxald=as.numeric(input$alder[2]),
                           erMann=as.numeric(input$erMann))
-      }, height=600, width=600 #height = function() {session$clientData$output_fordelinger_width}
+      }, height=700, width=700 #height = function() {session$clientData$output_fordelinger_width}
       )
       
       
@@ -416,7 +412,7 @@ server <- function(input, output, session) {
                             datoFra=input$datovalgSMR[1], datoTil=input$datovalgSMR[2],
                             minald=as.numeric(input$alderSMR[1]), maxald=as.numeric(input$alderSMR[2]),
                             erMann=as.numeric(input$erMannSMR))
-      }, heigth = 1000, width=700
+      }, heigth = 800, width=700
       )
       
       output$innMaate <- renderPlot({
@@ -424,7 +420,7 @@ server <- function(input, output, session) {
                            datoFra=input$datovalgInnMaate[1], datoTil=input$datovalgInnMaate[2],
                            minald=as.numeric(input$alderInnMaate[1]), maxald=as.numeric(input$alderInnMaate[2]),
                            erMann=as.numeric(input$erMannInnMaate))
-      }, height=800, width=800)
+      }, height=900, width=700)
 }
 
 
