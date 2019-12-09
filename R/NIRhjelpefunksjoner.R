ae24331eb71b5694104b3b221e83bd8b16e17922
Lena Ringstad Olsen <lenaringstado@gmail.com>
2019-12-09 11:08:43 +0100

    Lagt til logging av abbonement

--- R/NIRhjelpefunksjoner.R
+++ R/NIRhjelpefunksjoner.R
@@ -208,10 +208,18 @@ henteSamlerapporter <- function(filnavn, rnwFil, reshID=0,
 
 abonnement <- function(rnwFil, brukernavn='tullebukk', reshID=0, 
                                 datoFra=Sys.Date()-180, datoTil=Sys.Date()) {
-  Rpakke <- 'intensiv'
+  
+  #function(baseName, reshId, registryName,author, hospitalName, type) {
+    
+  raplog::subLogger(author = brukernavn, registryName = 'Norsk Intensivregister',
+                    reshId = reshID[[1]],
+                    msg = "Abonnement: månedsrapport")
+  # raplog::subLogger(author = author[[1]], registryName = registryName[[1]],
+  #                     reshId = reshId[[1]],
+  #                     msg = "Subscription report: stent/prosedyre")
   filbase <- substr(rnwFil, 1, nchar(rnwFil)-4)
   tmpFile <- paste0(filbase, Sys.Date(),'_',digest::digest(brukernavn), '.Rnw')
-  src <- normalizePath(system.file(rnwFil, package=Rpakke))
+  src <- normalizePath(system.file(rnwFil, package='intensiv'))
   # gå til tempdir. Har ikke skriverettigheter i arbeidskatalog
   owd <- setwd(tempdir())
   #on.exit(setwd(owd))
