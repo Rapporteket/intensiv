caefb49205105f520f19fd144e7668e91808ea15
Lena Ringstad Olsen <lenaringstado@gmail.com>
2022-06-17 07:48:27 +0200

    Tilrettelegg datasett til nettsider

--- R/NIRhjelpefunksjoner.R
+++ R/NIRhjelpefunksjoner.R
@@ -16,13 +16,13 @@ FinnReinnleggelser <- function(RegData, PasientID='PasientID'){
       #RegData må inneholde DateAdmittedIntensive, DateDischargedIntensive og PasientID
       #SJEKK Bare innleggelser fra 2016 som skal ha reinnleggelse??
       #RegData <- RegData[
-      #      as.POSIXlt(RegData$DateAdmittedIntensive, format="%Y-%m-%d %H:%M:%S") >= as.POSIXlt('2016-01-01'), ]
+      #     as.POSIXlt(RegData$DateAdmittedIntensive, format="%Y-%m-%d %H:%M:%S") >= as.POSIXlt('2016-01-01'), ]
       N <- dim(RegData)[1]
       RegData$PasientID <- RegData[ ,PasientID]
       #TabAntOpph <- table(RegData$PasientID) #Tar relativt lang tid.
       #TabFlereOpph <- TabAntOpph[TabAntOpph>1]
       #indPasFlereOpph <- which(RegData$PasientID %in% names(TabFlereOpph))  #Tar relativt lang tid.
-      RegDataSort <- RegData[order(RegData$PasientID, RegData$DateAdmittedIntensive,     #Denne tar mest tid
+      RegDataSort <- RegData[order(RegData$PasientID, RegData$DateAdmittedIntensive,   #Denne tar mest tid
                                    RegData$DateDischargedIntensive), ]
       #RegDataSort$AntOpph <- ave(RegDataSort$PasientID, RegDataSort$PasientID, FUN=length)
       RegDataSort$OpphNr <- ave(RegDataSort$PasientID, RegDataSort$PasientID, FUN=seq_along)
@@ -39,15 +39,15 @@ FinnReinnleggelser <- function(RegData, PasientID='PasientID'){
 
       #Div testing:
       #RegDataSort[indPasFlereOpph[1:20], c('OpphNr',"TidUtInn", 'ReshId','SmResh','Reinn', 'PasientID')]
-      # indNeg <- which(RegDataSort$TidUtInn < 0)
-      # TabDobbeltRegSjekk <- RegDataSort[sort(c(indNeg-1,indNeg)), ]
-      # write.table(TabDobbeltRegSjekk, file='TabDobbeltRegSjekk.csv', row.names = F, sep = ';')
-      # write.table(RegDataSort, file='RegDataSort.csv', row.names = F, sep = ';')
-      # RegDataSort[1:20,]
-      # table(RegDataSort$ReAdmitted)
-      # table(RegDataSort$Reinn)
-      # RegDataSort$PasientID[1:20,]
-      # RegDataSort$TidUtInn[indNeg[1:5]]
+      #indNeg <- which(RegDataSort$TidUtInn < 0)
+      #TabDobbeltRegSjekk <- RegDataSort[sort(c(indNeg-1,indNeg)), ]
+      #write.table(TabDobbeltRegSjekk, file='TabDobbeltRegSjekk.csv', row.names = F, sep = ';')
+      #write.table(RegDataSort, file='RegDataSort.csv', row.names = F, sep = ';')
+      #RegDataSort[1:20,]
+      #table(RegDataSort$ReAdmitted)
+      #table(RegDataSort$Reinn)
+      #RegDataSort$PasientID[1:20,]
+      #RegDataSort$TidUtInn[indNeg[1:5]]
       return(RegDataSort)
 }
 
@@ -73,10 +73,10 @@ SorterOgNavngiTidsEnhet <- function(RegData, tidsenhet='Aar', tab=0) {
 
       tidtxt <- switch(tidsenhet,
                        #Mnd = paste(substr(RegData$Aar[match(1:max(RegData$TidsEnhetSort), RegData$TidsEnhetSort)], 3,4),
-                        #           sprintf('%02.0f', RegData$Mnd[match(1:max(RegData$TidsEnhetSort), RegData$TidsEnhetSort)]), sep='.'),
+                        #          sprintf('%02.0f', RegData$Mnd[match(1:max(RegData$TidsEnhetSort), RegData$TidsEnhetSort)]), sep='.'),
                        #Mnd = RegData$MndAar[match(1:max(RegData$TidsEnhetSort), RegData$TidsEnhetSort)],
-                       # Mnd = format.Date(seq(from=min(as.Date(RegData$InnDato), na.rm = T),
-                       #                       to=max(as.Date(RegData$InnDato), na.rm = T), by='month'), format = '%b%y'),
+                       #Mnd = format.Date(seq(from=min(as.Date(RegData$InnDato), na.rm = T),
+                       #                      to=max(as.Date(RegData$InnDato), na.rm = T), by='month'), format = '%b%y'),
                        #Henter fullt månedsnavn og forkorter etterpå.
                        Mnd = format.Date(seq(from=lubridate::floor_date(as.Date(min(as.Date(RegData$InnDato), na.rm = T)), 'month'),
                                          to=max(as.Date(RegData$InnDato), na.rm = T), by='month'), format = '%B%y'), #Hele måneden
@@ -94,8 +94,8 @@ SorterOgNavngiTidsEnhet <- function(RegData, tidsenhet='Aar', tab=0) {
       #a <- factor(c(1:10,3,2,4,3,7,9,4), levels=1:11, labels = letters[1:11])
 #table(a)
 
-#      måned og år som faktor i riktig rekkefølge og med alle måneder inkludert
-#      RegData$MndAar <- factor(format(RegData $HovedDato, format='%b-%y'),
+#     måned og år som faktor i riktig rekkefølge og med alle måneder inkludert
+#     RegData$MndAar <- factor(format(RegData $HovedDato, format='%b-%y'),
       #levels = format(seq(as.Date(datoFra),as.Date(datoTil), by="month"), "%b-%y"))
 
       #RegData$TidsEnhet <- RegData$TidsEnhetSort
@@ -201,7 +201,7 @@ henteSamlerapporter <- function(filnavn, rnwFil, reshID=0,
     Rpakke <- ifelse(rnwFil==as.character('NIRinfluensa.Rnw'), 'intensivberedskap', 'intensiv')
     tmpFile <- paste0('tmp',rnwFil)
     src <- normalizePath(system.file(rnwFil, package=Rpakke))
-    # gå til tempdir. Har ikke skriverettigheter i arbeidskatalog
+    #gå til tempdir. Har ikke skriverettigheter i arbeidskatalog
     #owd <-
       setwd(tempdir())
     file.copy(src, tmpFile, overwrite = TRUE)
@@ -210,7 +210,7 @@ henteSamlerapporter <- function(filnavn, rnwFil, reshID=0,
 
     gc() #Opprydning gc-"garbage collection"
     file.copy(paste0(substr(tmpFile, 1, nchar(tmpFile)-3), 'pdf'), filnavn)
-    # file.rename(paste0(substr(tmpFile, 1, nchar(tmpFile)-3), 'pdf'), file)
+    #file.rename(paste0(substr(tmpFile, 1, nchar(tmpFile)-3), 'pdf'), file)
 }
 
 #' Funksjon som produserer rapporten som skal sendes til mottager.
@@ -230,147 +230,133 @@ abonnement <- function(rnwFil, brukernavn='tullebukk', reshID=0,
                                 datoFra=Sys.Date()-180, datoTil=Sys.Date()) {
 
   Rpakke <- ifelse(rnwFil==as.character('NIRinfluensa.Rnw'), 'intensivberedskap', 'intensiv')
-  # raplog::subLogger(author = brukernavn, registryName = 'Norsk Intensivregister',
-  #                   reshId = reshID,
-  #                   msg = paste0("1 starter Abonnement:", rnwFil ))
+  #raplog::subLogger(author = brukernavn, registryName = 'Norsk Intensivregister',
+  #                  reshId = reshID,
+  #                  msg = paste0("1 starter Abonnement:", rnwFil ))
   filbase <- substr(rnwFil, 1, nchar(rnwFil)-4)
   tmpFile <- paste0(filbase, Sys.Date(),'_',digest::digest(brukernavn), '.Rnw')
   src <- normalizePath(system.file(rnwFil, package=Rpakke))
-  # gå til tempdir. Har ikke skriverettigheter i arbeidskatalog
+  #gå til tempdir. Har ikke skriverettigheter i arbeidskatalog
   setwd(tempdir())
   dir <- getwd()
   file.copy(src, tmpFile, overwrite = TRUE)
-  # raplog::subLogger(author = brukernavn, registryName = 'Norsk Intensivregister',
-  #                   reshId = reshID,
-  #                   msg = paste0("2 Klar til å strikke", tmpFile))
+  #raplog::subLogger(author = brukernavn, registryName = 'Norsk Intensivregister',
+  #                  reshId = reshID,
+  #                  msg = paste0("2 Klar til å strikke", tmpFile))
   knitr::knit2pdf(input=tmpFile)
   utfil <- paste0(dir, '/', substr(tmpFile, 1, nchar(tmpFile)-3), 'pdf')
 
-  # raplog::subLogger(author = brukernavn, registryName = 'Norsk Intensivregister',
-  #                   reshId = reshID,
-  #                   msg = paste("3 Leverer: ", utfil))
+  #raplog::subLogger(author = brukernavn, registryName = 'Norsk Intensivregister',
+  #                  reshId = reshID,
+  #                  msg = paste("3 Leverer: ", utfil))
   return(utfil)
 }
 
 
-#' Generere data til resultatportalen eller sykehusviser
+#' Generere data til interaktive nettsider SKDE
 #' @param filUt tilnavn for utdatatabell (fjern?)
 #' @param valgtVar - beinsmLavPre, peropKompDura, sympVarighUtstr, p.t. 10 kvalitetsind.
 #' @param indID indikator-id, eks. 'ind1', 'ind2', osv.
-#' @param ResPort 1-hvis data til resultatportalen (standard), 0-data til SKDE-viser
 #' @inheritParams NIRUtvalgEnh
-#' @return Datafil til Resultatportalen
+#' @return Datafil til interaktive nettsider, SKDE
 #' @export
 
-dataTilOffVisning <- function(RegData = RegData, valgtVar, datoFra='2016-01-01', datoTil=Sys.Date(),
+dataTilOffVisning <- function(RegData = RegData, valgtVar, #datoFra='2016-01-01', datoTil=Sys.Date(),
                               aar=0,
-                              indID = 'indDummy', ResPort=1, filUt='dummy'){
+                              indID = 'indDummy', filUt='dummy'){
 
-  # figurtype <- switch(valgtVar,
-  #                     reinn = 'andelGrVar',
-  #                     respiratortidInvMoverf = 'gjsnGrVar')
-
-  resultatVariabler <- c('Aar', "ShNavn", "ReshId", "Variabel") #'KvalIndId',
-
-  filUt <- paste0('Intensiv_', ifelse(filUt=='dummy',  valgtVar, filUt), c('_SKDE', '_ResPort')[ResPort+1],'.csv')
+  filUt <- paste0('Intensiv_', filUt, '.csv')
+  resultatVariabler <- c('Aar', "ShNavn", "ReshId", "Variabel")
   DataVarSpes <- NIRVarTilrettelegg(RegData=RegData, valgtVar=valgtVar, figurtype = 'andelGrVar')$RegData
   RegDataUt <- NIRUtvalgEnh(RegData=DataVarSpes, aar = aar)$RegData[ , resultatVariabler]
-
-
-
-    if (ResPort == 1){
-    #Variabler: Aar	ReshId	Teller Ind1	Nevner Ind1	  AarID	   Indikator
-    #          2014	103469	  0	          1	       2014103469	  ind1
-    #RegDataUt <- RegData[,c('Aar', "ReshId", "ShNavn", "Variabel")]
-    RegDataUt<- dplyr::rename(RegDataUt, Teller = Variabel)
-    RegDataUt$AarID <- paste0(RegDataUt$Aar, RegDataUt$ReshId)
-    RegDataUt$Indikator <- indID
-    RegDataUt$Nevner <- 1
-  }
-
-  if (ResPort == 0){
-    #Variabler: year, orgnr, var, denominator, ind_id
-    #RegDataUt <- RegData #[,c('Aar', "ReshId", "Variabel")]
-    RegDataUt$ind_id <- indID
-    RegDataUt$denominator <- 1
-    # nytt navn = gammelt navn
+  #nytt navn = gammelt navn
     RegDataUt <- dplyr::rename(RegDataUt,
                                year = Aar,
                                var = Variabel)
 
     #Legge på orgID ("Sykehusviser")
     #ReshId	orgnr	RapporteketNavn	SKDEnavn
-  nyID <- c('102090'='974588951',               #AHUS - Intensiv
-  '4205696'='974588951',        #         AHUS - Postop
-  '111487'='974588951',         #                 Aker
-  '104450'='974631091',           #            Arendal
-  '4210053'='974795361',             #             Bodø
-  '103090'='974705788',               #          Bærum
-  '108897'='974116804',              #   Diakonhjemmet
-  '103620'='974631326',               #        Drammen
-  '108609'='974631768',                #       Elverum
-  '105282'='974744570',                #         Førde
-  '108618'='974632535',                 #       Gjøvik
-  '108610'='974724960',                 #        Hamar
-  '101858'='974795833',                 #   Hammerfest
-  '100180'='974316285',                 #  Haraldplass
-  '700617'='974795639',                 #      Harstad
-  '100273'='974724774',                  #   Haugesund
-  '109363'='974557746',              # Haukel. Brannsk
-  '112044'='974557746',             # Haukel. KSK Int.
-  '105048'='974557746',             #      Haukel. MIO
-  '106271'='974557746',             #   Haukel. Postop
-  '107717'='974557746',             #      Haukel. ROE
-  '106285'='974557746',             #      Haukel. TIO
-  '4209889'='974633752',             #      KalnesØstf.
-  '101830'='974795930',             #         Kirkenes
-  '4208715'='974631385',             #        Kongsberg
-  '114240'='974733013',              #    Kristiansand
-  '706078'='974746948',              #    Kristiansund
-  '102250'='974754118',              #        Levanger
-  '108626'='874632562',              #     Lillehammer
+  nyID <- c('102090'='974588951', #AHUS - Intensiv
+  '4215368' = '974588951', #AHUS - Kongsvinger
+  '4205696'='974588951', #AHUS - Postop
+  '111487'='974588951', #Aker
+  '104450'='974631091', #Arendal
+  '4210053'='974795361', #Bodø
+  '103090'='974705788', #Bærum
+  '108897'='974116804', #Diakonhjemmet
+  '103620'='974631326', #Drammen
+  '108609'='974631768', #Elverum
+  '105282'='974744570', #Førde
+  '108618'='974632535', #Gjøvik
+  '108610'='974724960', #Hamar
+  '101858'='974795833', #Hammerfest
+  '100180'='974316285', #Haraldplass
+  '700617'='974795639', #Harstad
+  '100273'='974724774', #Haugesund
+  '109636'='974557746', #Haukel. Brannsk, Riktig resh
+  '109363'='974557746', #Haukel. Brannsk, Feil resh
+  '112044'='974557746', #Haukel. KSK Int.
+  '105048'='974557746', #Haukel. MIO
+  '106271'='974557746', #Haukel. Postop
+  '107717'='974557746', #Haukel. ROE
+  '106285'='974557746', #Haukel. TIO
+  '4209889'='974633752', #KalnesØstf.
+  '101830'='974795930', #Kirkenes
+  '4208715'='974631385', #Kongsberg
+  '114240'='974733013', #Kristiansand
+  '706078'='974746948', #Kristiansund
+  '102250'='974754118', #Levanger
+  '108626'='874632562', #Lillehammer
   '4208892'='974207532', #Lovisenberg Diakonale Sykehus
-  '103015'='974795515',     #                Mo i Rana
-  '706079'='974745569',     #                    Molde
-  '103141'='974795485',      #                 Mosjøen
-  '4208039'='974633698',     #                     Moss
-  '105893'='974753898',     #                   Namsos
-  '700618'='974795396',     #                  Narvik
-  '705757'='974707152',     #         Radiumhospitalet
-  '705576'='874716782',     #         RH Barneintensiv
-  '705577'='874716782',     #            RH Gen Int 1
-  '706929'='874716782',     #            RH Gen Int 2
-  '700419'='874716782',      #               RH samlet
-  '103539'='974631407',       #              Ringerike
-  '103149'='974795477',        #          Sandnessjøen
-  '102026'='974633191',         #                Skien
-  '4201313'='974749025',          #   St. Olav Hovedint
-  '106572'='974749025',   # St. Olav Med int
-  '4205574'='974749025',  #St. Olav Med lunge
-  '114282'='974703300',         #            Stavanger
-  '700720'='974795787',      #Tromsø Intensivmedisinsk
-  '700619'='974795787',     #         Tromsø Kir. int.
-  '601302'='974795787',     #           Tromsø Med int
-  '700620'='974795787',     #            Tromsø Postop
-  '103948'='974589095',      #                Tønsberg
-  '109870'='974589095',     #     Ullevål Akuttmed Int
-  '111449'='974589095',      #        Ullevål Barneint
-  '109773'='974589095',     #          Ullevål Gen int
-  '109877'='974589095',     #        Ullevål Hjerte-PO
-  '4205969'='974589095',     #    Ullevål Hjertemed Int
-  '109779'='974589095',     #         Ullevål Nevroint
-  '109778'='974589095',     #           Ullevål Postop
-  '110867'='974795574',     #          Vesterål.Stokm.
-  '4209764'='974747545',     #                    Volda
-  '108308'='974747138',     #              Ålesund Kir
-  '102673'='974747138')     #              Ålesund Med
+  '103015'='974795515', #Mo i Rana
+  '706079'='974745569', #Molde
+  '103141'='974795485', #Mosjøen
+  '4208039'='974633698', #Moss
+  '105893'='974753898', # Namsos
+  '700618'='974795396', #Narvik
+  '705757'='974707152', #Radiumhospitalet
+  '705576'='874716782', #RH Barneintensiv
+  '705577'='874716782', #RH Gen Int 1
+  '706929'='874716782', #RH Gen Int 2
+  '700419'='874716782', #RH samlet
+  '103539'='974631407', #Ringerike
+  '103149'='974795477', #Sandnessjøen
+  '102026'='974633191', #Skien
+  '4201313'='974749025', #St. Olav Hovedint
+  '106572'='974749025', #St. Olav Med int
+  '4205574'='974749025', #St. Olav Med lunge
+  '105790' = '974749025', #St. Olavs hospital - Nevrointensiv
+  '114282'='974703300', #Stavanger
+  '701785'='974703300', #Stavanger univ.sjukehus - Postop. 1G
+  '700720'='974795787', #Tromsø Intensivmedisinsk
+  '700619'='974795787', #Tromsø Kir. int.
+  '601302'='974795787', #Tromsø Med int
+  '700620'='974795787', #Tromsø Postop
+  '103948'='974633574', #Tønsberg
+  '109870'='974589095', #Ullevål Akuttmed Int
+  '111449'='974589095', #Ullevål Barneint
+  '109773'='974589095', #Ullevål Gen int
+  '109877'='974589095', #Ullevål Hjerte-PO
+  '4205969'='974589095', #Ullevål Hjertemed Int
+  '109779'='974589095', #Ullevål Nevroint
+  '109778'='974589095', #Ullevål Postop
+  '110867'='974795574', #Vesterål.Stokm.
+  '4209764'='974747545', #Volda
+  '4213593'='974747545', #Volda
+  '108308'='974747138', #Ålesund Kir
+  '102673'='974747138') #Ålesund Med
 
+#nyResh <- setdiff(unique(RegDataUt$ReshId), names(nyID))
+#RegDataUt[match(nyResh, RegDataUt$ReshId), 'ShNavn']
   RegDataUt$orgnr <- as.character(nyID[as.character(RegDataUt$ReshId)])
-  RegDataUt <- RegDataUt[ ,c('year', 'orgnr', 'var', 'denominator', 'ind_id')]
-  #ShResh <- unique(RegDataUt[ ,c("orgnr", 'ReshId', "ShNavn")])
-}
+  RegDataUt$ind_id <- indID
+  RegDataUt$denominator <- 1
+  RegDataUt$context <- 'caregiver'
+
+  names(RegDataUt)
+  RegDataUt <- RegDataUt[ ,c('year', 'orgnr', 'var', 'denominator', 'ind_id', "context")]
 
 write.table(RegDataUt, file = filUt, sep = ';', row.names = F) #, fileEncoding = 'UTF-8')
-return(invisible(RegDataUt)) # return(IntensivKvalInd)
+return(invisible(RegDataUt)) #return(IntensivKvalInd)
 }
 
