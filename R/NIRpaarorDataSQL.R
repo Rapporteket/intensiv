a787274668869e9112040cc4d16460b7550fc69e
petterL <petter.linberg@helse-nord.no>
2026-05-11 13:31:14 +0200

    korrekt skjemanavn: spoerreskjema_om_paaroere_2

--- R/NIRpaarorDataSQL.R
+++ R/NIRpaarorDataSQL.R
@@ -118,7 +118,7 @@ SumScoreAllQuestions_2'
   datoTilP <- Sys.Date()
   queryP <- paste0('SELECT ',
                    varPaaror,
-                   ' FROM sporreskjema_om_paarorendes_tilfredshet_med_behandlingen_2
+                   ' FROM spoerreskjema_om_paaroere_2
                    WHERE FormVersionNumber > 13')
   # WHERE cast(FormDate as date) BETWEEN \'', datoFraP, '\' AND \'', datoTil, '\'')
 
